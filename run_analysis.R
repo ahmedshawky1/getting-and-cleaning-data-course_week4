7ce960736a6f14d9ca23c6fa8a8bcdb499d30190
ahmedshawky1 <34405473+ahmedshawky1@users.noreply.github.com>
2018-04-29 14:15:28 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,22 +2,27 @@ cat("\014")  # to clear screen
 library(reshape2)
 library(plyr)
 #1-Merges the training and the test sets to create one data set.
-  
-  #Read All Files
-subject_train <- read.table("./C4_4/train/subject_train.txt")
-subject_test <- read.table("./C4_4/test/subject_test.txt")
-X_train <- read.table("./C4_4/train/X_train.txt")
-X_test <- read.table("./C4_4/test/X_test.txt")
-Y_train <- read.table("./C4_4/train/y_train.txt")
-Y_test <- read.table("./C4_4/test/y_test.txt")
-activity_labels <- read.table("./C4_4/activity_labels.txt")
+
+#Read All Files
+subject_train <- read.table("./train/subject_train.txt")
+subject_test <- read.table("./test/subject_test.txt")
+X_train <- read.table("./train/X_train.txt")
+X_test <- read.table("./test/X_test.txt")
+Y_train <- read.table("./train/y_train.txt")
+Y_test <- read.table("./test/y_test.txt")
+
+activity_labels <- read.table("./activity_labels.txt")
 colnames(activity_labels)<-c("Activity_ID","Activity_Name")
-print(activity_labels)
-features <- read.table("./C4_4/features.txt")
+features <- read.table("./features.txt")
+names(subject_train) <- "subject"
+names(subject_test) <- "subject"
 names(X_train) <- features$V2
 names(X_test) <- features$V2
-ALL_train <- cbind(subject_train, y_train, X_train)
-ALL_test <- cbind(subject_test, y_test, X_test)
+names(Y_train) <- "Activity_ID"
+names(Y_test) <- "Activity_ID"
+
+ALL_train <- cbind(subject_train, Y_train, X_train)
+ALL_test <- cbind(subject_test, Y_test, X_test)
 ALL_DATA <- rbind(ALL_train, ALL_test);  #req 1
 
 
@@ -36,13 +41,14 @@ ALL_DATA_Filtered <- ALL_DATA[, req_col] #req2
 
 final <- join(ALL_DATA_Filtered, activity_labels, by = "Activity_ID", match = "first")
 
-#final <-final[,-1] #req 3 ,4
+
 
 
 #5-From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-write.table(final,file="data.txt")
-
+melted <- melt(final, id=c("subject","Activity_Name"))
+tidydata <- dcast(melted, subject+Activity_Name ~ variable, mean)
+write.csv(tidydata, "tidydata.txt", row.names=FALSE)
 
 
 
